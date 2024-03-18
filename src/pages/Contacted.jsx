d3d639d93716eac0e74ac373581bdd05553002b1
barakk123 <Barakk123@gmail.com>
2024-03-18 22:12:51 +0200

    contacted works

--- src/pages/Contacted.jsx
+++ src/pages/Contacted.jsx
@@ -1,55 +1,60 @@
-import React, { useState, useEffect } from "react";
-import ContactedListContainer from "../components/ContactedListComponent";
-import { Grid, Typography } from "@mui/material";
+import { useState, useEffect } from "react";
+import Box from "@mui/material/Box";
+import BabysitterCard from "../components/BabysitterCard";
 import { useAuth } from "../AuthContext";
-import { get } from "../api";
-import ErrorBoundary from "./ErrorBoundary";
+import { useDataContext } from "../context/DataContext";
+
 const Contacted = () => {
+  const data = useDataContext();
   const [contacted, setContacted] = useState([]);
   const { user } = useAuth();
 
-  const fetchContacted = async () => {
-    if (user.userData.user?.userType === "parent") {
-      const parent = await get("parents", user.id);
-      console.log("parent:", parent);
-      const contacted = parent.contacted;
-      console.log("contacted:", contacted);
-      setContacted(contacted);
-    } else {
-      const babysitter = await get("babysitters", user.id);
-      console.log("babysitter:", babysitter);
-      const contacted = babysitter.contacted;
-      console.log("contacted:", contacted);
-      setContacted(contacted);
-    }
-  };
-
   useEffect(() => {
-    if (user && !contacted) {
-      fetchContacted();
-      console.log("contacted:", contacted);
-    }
-  });
+      const contacted2 = user.userData.contacted || [];
+      setContacted(contacted2);
+
+  }, [user, data]);
+
   return (
-    <div>
-      <h1>Contacted</h1>
-      <Grid container spacing={2} className="ml-4">
-        {contacted ? (
-          contacted.map((item) => (
-            <Grid item key={item.id} xs={4} sm={4} md={2} lg={2}>
-              {/* Adjust the Grid item sizes based on your layout needs */}
-              <ErrorBoundary errorType="modal">
-                <ContactedListContainer contactedData={contacted} />
-              </ErrorBoundary>
-            </Grid>
-          ))
-        ) : (
-          <Typography variant="h6" className="m-4">
-            No contacted babysitters
-          </Typography>
-        )}
-      </Grid>
-    </div>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        gap: "20px",
+        flexWrap: "wrap",
+        padding: "20px 10px",
+        justifyContent: "center",
+        alignItems: "start",
+        backgroundColor: "#F8F7F1",
+        minHeight: "100vh",
+      }}
+    >
+      {contacted.length > 0 ? (
+        contacted.map(
+          (contacted) => (
+            (
+              <BabysitterCard
+                key={contacted.babysitter.id}
+                {...{"babysitter": contacted.babysitter}}
+              />
+            )
+          ),
+        )
+      ) : (
+        <div
+          style={{
+            flexGrow: 1,
+            height: "100vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <p>No contacted</p>
+        </div>
+      )}
+    </Box>
   );
 };
+
 export default Contacted;
